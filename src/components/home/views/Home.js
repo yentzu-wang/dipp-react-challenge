8bf9dcb56c06a8bace79b3c6644484b5e75382e0
Frank <frank7300228@gmail.com>
2020-02-22 16:44:01 +0800

    Update components - Home, NavigationBar

--- src/components/home/views/Home.js
+++ src/components/home/views/Home.js
@@ -1,7 +1,35 @@
 import React from "react"
+import { useHistory } from "react-router-dom"
 
 const Home = () => {
-  return <div>Home</div>
+  const history = useHistory()
+
+  return (
+    <>
+      <div className="home">
+        <div className="heading-primary">
+          <div className="heading-primary--main">Select your app</div>
+        </div>
+        <div className="heading-secondary">
+          <div className="home__link" onClick={() => history.push("/weather")}>
+            Weather
+          </div>
+        </div>
+        <div className="heading-secondary">
+          <div className="home__link" onClick={() => history.push("/image-ad")}>
+            Image Ad
+          </div>
+        </div>
+      </div>
+      <div className="home__info">
+        by Frank Wang
+        <div>
+          Feel free to send me any feedback through{" "}
+          <a href="mailto:frank7300228@hotmail.com">email</a>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Home
